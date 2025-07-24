d770f63704f18d81c321002d674148c322fe16be
Addryann <adryanvaldez1@gmail.com>
2025-07-24 00:57:43 -0700

    Changed some things

--- src/main/java/net/valdev/formula1Mod/Formula1Mod.java
+++ src/main/java/net/valdev/formula1Mod/Formula1Mod.java
@@ -1,5 +1,6 @@
 package net.valdev.formula1Mod;
 
+import net.valdev.formula1Mod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -35,6 +36,8 @@ public class Formula1Mod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
---
+++ src/main/java/net/valdev/formula1Mod/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.valdev.formula1Mod.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.valdev.formula1Mod.Formula1Mod;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Formula1Mod.MOD_ID);
+
+    public static final DeferredItem<Item> PITCOIN = ITEMS.register("pitcoin",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
